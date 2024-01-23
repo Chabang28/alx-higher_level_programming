b3457c0104b7029d0c913ead72dfbc253c41f0d0
Chabang28 <chabangmnisi@gmail.com>
2024-01-23 02:26:24 -0800

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
 
-my_list = [1, 2, 3, 4, 5]
+def safe_print_list(my_list=[], x=0):
+    """prints x elements of a list"""
+    """returns real number of elements printed"""
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+    # if we should print nothing
+    if x == 0:
+        print()
+        return 0
+
+    # track elements printed
+    elements_printed = 0
+
+    # iterate through list and print elements
+    for element in my_list:
+        try:
+            print(f'{element}', end='')
+            elements_printed += 1
+        except exception as e:
+            print(e)
+        if elements_printed == x:
+            print()
+            return elements_printed
+    print()
+    return elements_printed
