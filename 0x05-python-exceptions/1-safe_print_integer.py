1ff79c9e374be3399c6a23cbeb01ecd3b8fc2d28
Chabang28 <chabangmnisi@gmail.com>
2024-01-23 02:48:13 -0800

    Print and count integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+def safe_print_integer(value):
+    """prints an integer with "{:d}".format()"""
+    """returns true if integer and false if not"""
 
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        return False
