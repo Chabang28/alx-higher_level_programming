2b8c513849791e3f8114b2477dec9efb22614ae8
Chabang28 <chabangmnisi@gmail.com>
2024-02-14 14:38:08 -0800

    variables

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    '''replace all occurrences of an element by another in a new list'''
+
+    new_list = [replace if elem == search else elem for elem in my_list]
+    '''
+    for elem in my_list:
+        if elem == search:
+            new_list.append(replace)
+        else:
+            new_list.append(elem)
+    '''
+    return new_list
